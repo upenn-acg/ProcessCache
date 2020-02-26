63f72caca68f005af411d53a09d4c93234f116f7
Tessa Fan <tessafan@huntsman-ve703-10944.apn.wlan.private.upenn.edu>
2020-02-26 12:17:14 -0500

    Updated conflict_tracer tests

--- tests/test_binutils.rs
+++ tests/test_binutils.rs
@@ -1,14 +1,53 @@
 use std::process::Command;
+use std::io::{self, Write};
 
 // NB: cwd when running these tests is ProcessCache/
 
+// pwd test
 #[test]
 fn pwd() {
     let mut cmd = Command::new("target/debug/conflict_tracer");
     cmd.arg("pwd"); 
-    cmd.status().expect("error running pwd");
+    assert!(cmd.status().expect("incorrect running pwd").success());
 }
 
-// TODO: sleep 1
-// TODO: ls -lh /usr/lib
-// TODO: time pwd
+//sleep 1 test
+#[test]
+fn sleep1() {
+    let mut cmd = Command::new("target/debug/conflict_tracer");
+    cmd.args(&["sleep", "1"]); 
+    let output = cmd.output().expect("incorrect running sleep 1");
+    println!("status: {}", output.status);
+    io::stdout().write_all(&output.stdout).unwrap();
+    io::stderr().write_all(&output.stdout).unwrap();
+    assert!(output.status.success());
+}
+
+//ls -lh /usr/lib
+#[test]
+fn usr_lib() {
+    let output = Command::new("target/debug/conflict_tracer")
+					.args(&["ls", "/usr/lib"])
+    				.output()
+    				.expect("error running usr_lib");
+
+    println!("status: {}", output.status);
+    io::stdout().write_all(&output.stdout).unwrap();
+    io::stdout().write_all(&output.stderr).unwrap();
+    assert!(output.status.success());
+}
+
+//time pwd
+#[test]
+fn time_pwd() {
+    let mut cmd = Command::new("target/debug/conflict_tracer");
+    cmd.arg("time"); 
+    cmd.arg("pwd");
+    assert!(cmd.status().expect("error running time & pwd").success());
+}
+
+//
+
+
+
+
