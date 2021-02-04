fe7168743694646c75f39a5423f9c9546f8cff38
krs85 <kelly.shiptoski@gmail.com>
2021-02-04 15:22:16 -0500

    Move log and events out of execution.rs

--- src/execution.rs
+++ src/execution.rs
@@ -18,6 +18,7 @@ use std::io::BufWriter;
 use std::io::Write;
 use std::rc::Rc;
 
+use crate::log::{ExecveEvent, ForkEvent, Mode, OpenEvent, StatEvent};
 use crate::regs::Regs;
 use crate::regs::Unmodified;
 use crate::tracer::TraceEvent;
@@ -30,138 +31,6 @@ use tracing::{debug, error, info, span, trace, Level};
 use anyhow::{anyhow, bail, Context, Result};
 use nix::errno::errno;
 
-enum Mode {
-    ReadOnly,
-    ReadWrite,
-    WriteOnly,
-}
-
-pub struct ExecveEvent {
-    args: Vec<String>,
-    path_name: String,
-}
-
-impl fmt::Display for ExecveEvent {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut log_string = String::new();
-
-        log_string.push_str(&format!(
-            "Execve event: {:?}, {:?}\n",
-            self.path_name, self.args
-        ));
-
-        write!(f, "{}", log_string)
-    }
-}
-pub struct ForkEvent {
-    child_pid: Pid,
-    current_pid: Pid,
-}
-
-impl fmt::Display for ForkEvent {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut log_string = String::new();
-
-        log_string.push_str(&format!(
-            "Fork Event. Creating task for new child: {}. Parent pid is: {}\n",
-            self.child_pid, self.current_pid
-        ));
-
-        write!(f, "{}", log_string)
-    }
-}
-
-pub struct OpenEvent {
-    fd: i32,
-    inode: Option<u64>,
-    is_create: bool,
-    mode: Mode,
-    path: String, // Full path if possible, else relative path.
-    pid: Pid,
-    syscall_name: String,
-}
-
-impl fmt::Display for OpenEvent {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut log_string = String::new();
-        if self.is_create {
-            log_string.push_str(&format!(
-                "File create event ({}): opened for writing. ",
-                self.syscall_name
-            ));
-        } else {
-            log_string.push_str(&format!("File open event ({}): ", self.syscall_name));
-        }
-
-        match self.mode {
-            Mode::ReadOnly => log_string.push_str("File opened for reading. "),
-            Mode::WriteOnly => log_string.push_str("File opened for writing. "),
-            Mode::ReadWrite => log_string.push_str("File open for reading/writing. "),
-        }
-
-        log_string.push_str(&format!(
-            "Fd: {}, Path: {}, Pid: {}, ",
-            self.fd, self.path, self.pid
-        ));
-
-        if let Some(ino) = self.inode {
-            log_string.push_str(&format!("Inode: {} \n", ino));
-        } else {
-            log_string.push('\n');
-        }
-
-        write!(f, "{}", log_string)
-    }
-}
-
-pub struct StatEvent {
-    fd: Option<i32>,
-    inode: Option<u64>,
-    is_symbolic_link: bool,
-    path: Option<String>,
-    pid: Pid,
-    success: bool,
-    syscall_name: String,
-}
-
-impl fmt::Display for StatEvent {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut log_string = String::new();
-
-        log_string.push_str(&format!(
-            "File stat event ({}): Pid: {}, ",
-            self.syscall_name, self.pid
-        ));
-
-        if self.success {
-            log_string.push_str("Success, ");
-        } else {
-            log_string.push_str("Fail, ");
-        }
-
-        if self.is_symbolic_link {
-            log_string.push_str("Symlink, ");
-        } else {
-            log_string.push_str("Hardlink, ");
-        }
-
-        if let Some(file_name) = self.path.clone() {
-            log_string.push_str(&format!("Path: \"{}\", ", file_name));
-        }
-
-        if let Some(file_d) = self.fd {
-            log_string.push_str(&format!("Fd: {}, ", file_d));
-        }
-
-        if let Some(ino) = self.inode {
-            log_string.push_str(&format!("Inode: {}\n", ino));
-        } else {
-            log_string.push('\n');
-        }
-
-        write!(f, "{}", log_string)
-    }
-}
 pub struct Log {
     log: BufWriter<File>,
     print_all_syscalls: bool,
@@ -366,10 +235,7 @@ pub async fn do_run_process(
                     debug!("Parent pid is: {}", tracer.curr_proc);
                 });
 
-                let fork_event = ForkEvent {
-                    child_pid: child,
-                    current_pid: tracer.curr_proc,
-                };
+                let fork_event = ForkEvent::new(child, tracer.curr_proc);
                 log_writer.add_event(&fork_event)?;
 
                 // Recursively call run process to handle the new child process!
@@ -471,15 +337,15 @@ fn handle_open(
         };
 
         let (path, inode) = path_and_inode;
-        let open_event = OpenEvent {
+        let open_event = OpenEvent::new(
             fd,
             inode,
             is_create,
             mode,
             path,
             pid,
-            syscall_name: String::from(syscall_name),
-        };
+            String::from(syscall_name),
+        );
 
         log_writer.add_event(&open_event)?;
     }
@@ -540,15 +406,15 @@ fn handle_stat(
             Some(stat_struct.st_ino)
         };
 
-        let stat_event = StatEvent {
+        let stat_event = StatEvent::new(
             fd,
             inode,
             is_symbolic_link,
-            path: path_name,
+            path_name,
             pid,
             success,
-            syscall_name: String::from(syscall_name),
-        };
+            String::from(syscall_name),
+        );
 
         log_writer.add_event(&stat_event)?;
     }
@@ -578,7 +444,7 @@ async fn handle_execve(
             debug!("execve(\"{:?}\", {:?})", path_name, args);
             trace!("envp={:?}", envp);
         });
-        let execve_event = ExecveEvent { args, path_name };
+        let execve_event = ExecveEvent::new(args, path_name);
         log_writer.add_event(&execve_event)?;
     }
 
