08bb4afa5c98220d923a965ecca477e0783c3bed
krs85 <kelly.shiptoski@gmail.com>
2022-06-09 15:51:10 -0400

    delete unnecessary files, remove hardcoded paths

--- simplest_example.rs
+++
@@ -1,8 +0,0 @@
-use std::process::Command;
-
-fn main() {
-    let exec = String::from("./target/debug/simplest_ex_child");
-
-    let mut job = Command::new(exec).spawn().expect("failed to run job");
-    let _ = job.wait().expect("couldn't wait on job");
-}
