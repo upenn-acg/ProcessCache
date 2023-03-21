2ba578d5746d4949ef3fdb8e9d0487ecb11675c7
krs85 <kelly.shiptoski@gmail.com>
2023-03-21 11:31:36 -0400

    remove test binutils

--- tests/test_binutils.rs
+++ tests/test_binutils.rs
@@ -1,37 +1,37 @@
-use std::io::{self, Write};
-use std::process::Command;
+// use std::io::{self, Write};
+// use std::process::Command;
 
-// NB: cwd when running these tests is ProcessCache/
+// // NB: cwd when running these tests is ProcessCache/
 
-// pwd test
-#[test]
-fn pwd() -> io::Result<()> {
-    let mut cmd = Command::new("./target/debug/process_cache");
-    cmd.arg("pwd");
-    assert!(cmd.status()?.success());
-    Ok(())
-}
+// // pwd test
+// #[test]
+// fn pwd() -> io::Result<()> {
+//     let mut cmd = Command::new("./target/debug/process_cache");
+//     cmd.arg("pwd");
+//     assert!(cmd.status()?.success());
+//     Ok(())
+// }
 
-//ls -lh /usr/lib
-#[test]
-fn usr_lib() -> io::Result<()> {
-    let output = Command::new("./target/debug/process_cache")
-        .args(["ls", "/usr/lib"])
-        .output()?;
+// //ls -lh /usr/lib
+// #[test]
+// fn usr_lib() -> io::Result<()> {
+//     let output = Command::new("./target/debug/process_cache")
+//         .args(["ls", "/usr/lib"])
+//         .output()?;
 
-    println!("status: {}", output.status);
-    io::stdout().write_all(&output.stdout)?;
-    io::stdout().write_all(&output.stderr)?;
-    assert!(output.status.success());
-    Ok(())
-}
+//     println!("status: {}", output.status);
+//     io::stdout().write_all(&output.stdout)?;
+//     io::stdout().write_all(&output.stderr)?;
+//     assert!(output.status.success());
+//     Ok(())
+// }
 
-//time pwd
-#[test]
-fn time_pwd() -> io::Result<()> {
-    let mut cmd = Command::new("./target/debug/process_cache");
-    cmd.arg("time");
-    cmd.arg("pwd");
-    assert!(cmd.status()?.success());
-    Ok(())
-}
+// //time pwd
+// #[test]
+// fn time_pwd() -> io::Result<()> {
+//     let mut cmd = Command::new("./target/debug/process_cache");
+//     cmd.arg("time");
+//     cmd.arg("pwd");
+//     assert!(cmd.status()?.success());
+//     Ok(())
+// }
